b5b8f558a4203e40fddf2012fb0531c5b1f42cd2
Arjun <arjun140702@gmail.com>
2022-07-15 22:41:49 +0530

    Setup UI libraries

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native"
 
+import * as eva from "@eva-design/eva"
+import { ApplicationProvider } from "@ui-kitten/components"
+
 import MainStack from "./src/navigation/Main"
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <MainStack />
-        </NavigationContainer>
+        <ApplicationProvider {...eva} theme={eva.light}>
+            <NavigationContainer>
+                <MainStack />
+            </NavigationContainer>
+        </ApplicationProvider>
     )
 }
 
